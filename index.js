76e0b1f314fdc74e06818bf93fc6da7f76dc030d
mattbechtel1 <bechma02@gmail.com>
2020-01-02 16:49:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(string) {
+    const uncapitalizedWords = ['a', 'an', 'but', 'of', 'and', 'for', 'by', 'at', 'the', 'from']
+    let words = string.split(' ')
+    return words.map(function(word, index) {
+      if (index === 0) {
+        return Formatter.capitalize(word)
+      } else if (!uncapitalizedWords.find(function(uncappedWord) {
+          return uncappedWord === word
+      })) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
 }
