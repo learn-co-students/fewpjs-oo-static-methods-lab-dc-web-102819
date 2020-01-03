c6d1fe56fe064b0987890532572b168968fb7f25
Kyle <kylepcole@gmail.com>
2020-01-03 10:17:43 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+  	return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+  	return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+  	const notToTitleCase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+  	const array = string.split(' ')
+  	let output = []
+
+  	array.forEach(word => {
+	  	debugger
+  		if (notToTitleCase.includes(word)) {
+  			output.push(word)
+  			debugger
+  		} else {
+  			output.push(Formatter.capitalize(word))
+  			debugger
+  		}
+  	})
+  	console.log(Formatter.capitalize(output.join(' ')))
+  	return Formatter.capitalize(output.join(' '))
+  }
 }
